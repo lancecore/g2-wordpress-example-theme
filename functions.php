2592f920f4595ce19d6b48b252ed0801c5a65415
Lance Boer <lance@Lances-MacBook-Pro-2.local>
2021-09-02 12:21:37 -0700

    Sept 2 2021 updates

---
+++ functions.php
@@ -0,0 +1,12 @@
+<?php
+add_action( 'widgets_init', 'themename_widgets_init' );
+function themename_widgets_init() {
+	register_sidebar( array(
+		'name'          => __( 'Awesome Sidebar', 'theme_name' ),
+		'id'            => 'sidebar-1',
+		'before_widget' => '<div id="%1$s" class="widget %2$s awesome">',
+		'after_widget'  => '</div>',
+		'before_title'  => '<h3 class="widget-title">',
+		'after_title'   => '</h3>',
+	) );
+}
--- home.php
+++ home.php
@@ -10,33 +10,85 @@
 
   <div class="row mb-2">
     <div class="col-md-6">
-      <div class="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
+      <div class="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm position-relative">
+        <?php
+        // WP_Query arguments
+        $args = array(
+          'posts_per_page'         => '1',
+          'order'                  => 'DESC',
+          'orderby'                => 'date',
+          'category_name'          => 'dogs',
+        );
+        
+        // The Query
+        $dog_query = new WP_Query( $args );
+        
+        // The Loop
+        if ( $dog_query->have_posts() ) {
+          while ( $dog_query->have_posts() ) {
+            $dog_query->the_post();
+            // do something
+        ?>
         <div class="col p-4 d-flex flex-column position-static">
-          <strong class="d-inline-block mb-2 text-primary">World</strong>
-          <h3 class="mb-0">Featured post</h3>
-          <div class="mb-1 text-muted">Nov 12</div>
-          <p class="card-text mb-auto">This is a wider card with supporting text below as a natural lead-in to additional content.</p>
-          <a href="#" class="stretched-link">Continue reading</a>
+          <strong class="d-inline-block mb-2 text-primary"><?php the_category(); ?></strong>
+          <h3 class="mb-0"><?php the_title(); ?></h3>
+          <div class="mb-1 text-muted"><?php the_date(); ?></div>
+          <p class="card-text mb-auto"><?php the_excerpt(); ?></p>
+          <a href="<?php the_permalink(); ?>" class="stretched-link">Continue reading</a>
         </div>
         <div class="col-auto d-none d-lg-block">
           <svg class="bd-placeholder-img" width="200" height="250" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
-
         </div>
+        <?php
+          }
+        } else {
+          // no posts found
+        }
+        
+        // Restore original Post Data
+        wp_reset_postdata();
+        ?>
       </div>
     </div>
     <div class="col-md-6">
-      <div class="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
-        <div class="col p-4 d-flex flex-column position-static">
-          <strong class="d-inline-block mb-2 text-success">Design</strong>
-          <h3 class="mb-0">Post title</h3>
-          <div class="mb-1 text-muted">Nov 11</div>
-          <p class="mb-auto">This is a wider card with supporting text below as a natural lead-in to additional content.</p>
-          <a href="#" class="stretched-link">Continue reading</a>
-        </div>
-        <div class="col-auto d-none d-lg-block">
-          <svg class="bd-placeholder-img" width="200" height="250" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
-
-        </div>
+      <div class="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm position-relative">
+      <?php
+      // WP_Query arguments
+      $args_dog = array(
+        'posts_per_page'         => '1',
+        'order'                  => 'DESC',
+        'orderby'                => 'date',
+        'category_name'          => 'cats',
+      );
+      
+      // The Query
+      $cat_query = new WP_Query( $args_dog );
+      
+      // The Loop
+      if ( $cat_query->have_posts() ) {
+        while ( $cat_query->have_posts() ) {
+        $cat_query->the_post();
+        // do something
+      ?>
+      <div class="col p-4 d-flex flex-column position-static">
+        <strong class="d-inline-block mb-2 text-primary"><?php the_category(); ?></strong>
+        <h3 class="mb-0"><?php the_title(); ?></h3>
+        <div class="mb-1 text-muted"><?php the_date(); ?></div>
+        <p class="card-text mb-auto"><?php the_excerpt(); ?></p>
+        <a href="<?php the_permalink(); ?>" class="stretched-link">Continue reading</a>
+      </div>
+      <div class="col-auto d-none d-lg-block">
+        <svg class="bd-placeholder-img" width="200" height="250" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
+      </div>
+      <?php
+        }
+      } else {
+        // no posts found
+      }
+      
+      // Restore original Post Data
+      wp_reset_postdata();
+      ?>
       </div>
     </div>
   </div>
